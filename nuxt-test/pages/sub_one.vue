4ec927611ee2e9696b818eaab8ae636cf1dffd5c
cruel-32 <cruel32@nate.com>
2019-02-27 12:39:21 +0900

    asyncData 테스트

--- nuxt-test/pages/sub_one.vue
+++ nuxt-test/pages/sub_one.vue
@@ -10,8 +10,8 @@
       </h2>
       <div class="links">
         <a
-          href="/sub_one"
-          class="button--green">sub_one test</a>
+          href="/"
+          class="button--green">home</a>
         <a
           href="https://nuxtjs.org/"
           target="_blank"
@@ -21,14 +21,39 @@
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
+      <div>
+        {{user}}
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import axios from 'axios';
 
 export default {
+  //asyncData은 비동기로 데이터를 긁어온뒤에 페이지를 렌더링함...
+  //기존 서버사이드 렌더링처럼 spa 대비 초기 화면 로드가 느리다는 단점이 있음.
+  //하지만 한번 로드 이후로는 캐싱이 먹어서인지 빠름...
+  async asyncData ({ params, error }) {
+    const {data} = await axios.get('http://localhost:4000/user'); 
+    const {dummy } = await new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve({
+          dummy : {
+            dummyNumber : 3000,
+            dummyName : 'abcd',
+          }
+        })
+      },5000)
+    })
+    console.log('data : ', data);
+    console.log('dummy : ', dummy);
+    return {
+      user : Object.assign(dummy,data)
+    }
+  },
   components: {
     AppLogo
   }
